b05bb3a362ec8122f6898d150eb7a4f49b590e69
WataruTakahagi <watarut@outlook.com>
2020-11-26 17:10:19 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Haruka Nakano, Wataru Takahagi, Sota Niki',
     author_email='haruka.n@g.ecc.u-tokyo.ac.jp',
     url='https://github.com/utokyo-hirata-lab/iquant3d_mineral',
-    py_modules=['iquant3Dm'],
+    py_modules=['iQuant3Dm'],
     description='Multi-elemnt 3D-imaging software for minerals without GUI',
     keywords='visualization data-science',
     classifiers=[
