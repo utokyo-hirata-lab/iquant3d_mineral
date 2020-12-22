81ba03eda6ab0a69369e8be5cc0a91649ca440f0
HarukaNakano <52775171+HarukaNakano@users.noreply.github.com>
2020-12-22 15:59:53 +0900

    Add files via upload

--- iquant3d_mineral.py
+++ iquant3d_mineral.py
@@ -4,6 +4,7 @@ import os.path
 import sys
 import csv
 import warnings
+import itertools
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -16,7 +17,7 @@ import plotly.graph_objects as go
 import plotly.figure_factory as ff
 
 class iq3t_m():
-    def __init__(self, folder, filelist=[], elements=[], data=[],offset = 3, length = 122,nb_line = 17,washout = 30):
+    def __init__(self, folder, filelist=[], elements=[], data=[],std_element = "29Si", offset = 3, length = 122,nb_line = 17,washout = 30):
         self.folder = folder
         self.filelist = filelist
         self.elements = elements
@@ -25,6 +26,7 @@ class iq3t_m():
         self.length = length
         self.nb_line = nb_line
         self.washout = washout
+        self.std_element = std_element
 
     def csv_list(self, folder):
         filepath = glob.glob(folder + "/*.csv")
@@ -37,6 +39,17 @@ class iq3t_m():
         elements = names[1:len(names)-1]
         self.elements = elements
         return elements
+
+    def translate_data(self,file,std_element,elements,gasdata,stddata,concentration):
+        df_gas = pd.read_csv(gasdata,skiprows=[14],header=12,dtype='float64',index_col = 0, low_memory=False)
+        df_std = pd.read_csv(stddata,skiprows=[14],header=12,dtype='float64',index_col = 0, low_memory=False)
+        df_intencity = pd.read_csv(concentration,header = 0,low_memory = False)
+        std_intencity = df_std - df_gas.mean()
+        mean_data = std_intencity.mean()
+        new_file = file[elements]*(28*1000000/190)*(df_intencity[elements][0]/df_intencity[std_element][0])/(file[std_element]*(mean_data[elements]/mean_data[std_element]))
+        return new_file
+
+
 
     def imaging(self, folder, file, element, offset, length, nb_line, washout):
         print("Analysis of " + file + "_" + element)
@@ -78,6 +91,51 @@ class iq3t_m():
         plt.style.use('default')
         plt.close()
 
+    def imaging_quantum(self, folder, file, element, offset, length, nb_line, washout,gasdata,stddata,concentration, std_element = "29Si"):
+        print("Analysis of " + file + "_" + element)
+        row_data = pd.read_csv(file, dtype="float64", skiprows=[14], header=12,low_memory=False)
+        data = self.translate_data(row_data,std_element,element,gasdata,stddata,concentration)
+        data.index = row_data["Time"]
+        df = data.reset_index()
+        df.columns = ["Time",element]
+        #print(df.head(10))
+        ts = offset
+        line_num = 0
+        merged_line = pd.DataFrame()
+        fig = plt.figure(figsize=(15, 3))
+        ax = fig.add_subplot(111)
+        plt.rcParams['lines.linewidth'] = 0.3
+        plt.plot(df["Time"], df[element], color='black', linewidth=0.3)
+
+        for i in range(len(df[element])):
+            line_num += 1
+            y = df.query("@ts <Time < (@ts + @length)")[element]
+            merged_line['line'+str(line_num)] = pd.Series(list(y))
+            ax.axvspan(df.iloc[y.index[0]]["Time"], df.iloc[y.index[-1]]["Time"], color="lightgray")
+            line_length = length + washout
+            ts += line_length
+            if line_num == nb_line:
+                break
+
+        outname = file.split('.')[0]+'_'+element+'_q_signal.pdf'
+        plt.savefig(outname)
+        plt.close()
+
+        outname = file.split('.')[0]+'_'+element+'_q.xlsx'
+        merged_line.T.to_excel(outname, sheet_name=element)
+
+        sns.set()
+        plt.style.use('dark_background')
+        plt.figure()
+        ax = plt.subplot(111)
+        sns.heatmap(merged_line.T, cmap='jet', xticklabels=False, yticklabels=False, ax=ax, cbar=True, robust=True)
+        plt.tight_layout()
+        outname = file.split('.')[0]+'_'+element+'_q_mapping.png'
+        plt.savefig(outname)
+        plt.style.use('default')
+        plt.close()
+
+
     def moving(self, folder):
         dirname = self.folder+'/result'
         if os.path.isdir(dirname) is False:
@@ -107,12 +165,23 @@ class iq3t_m():
     def ccf_table(self,file,elements, std, threshold):
         list = []
         ind, col = elements, elements
+        x = len(elements)
+        ccf_element = [np.nan]*x
+        for p in itertools.product(range(x), repeat=2):#デカルト積
+            ccf_element[p[1]] = self.ccf(file,elements[p[0]],elements[p[1]], std, threshold)
+            print(ccf_element[p[1]])
+            list.append(ccf_element[p[1]])
+
+        new_list = np.array(list).reshape(x,x).tolist()
+
+        """
+        2重ループ
         for i in range(len(elements)):
             ccf_element = [np.nan]*len(elements)
             for j in range(i,len(elements)):
                 ccf_element[j] = self.ccf(file,elements[i],elements[j], std, threshold)
-            list.append(ccf_element)
-        df = pd.DataFrame(list,index = ind, columns = col)
+        """
+        df = pd.DataFrame(new_list,index = ind, columns = col)
         return df.T
 
     def execlusion(self, offset, length, nb_line, washout):
@@ -121,12 +190,53 @@ class iq3t_m():
         elements = self.element_list(l)
         [[self.imaging(self.folder, file, i, offset, length, nb_line, washout) for i in elements] for file in l]
         self.moving(self.folder)
-        #offset=5, length=103, nb_line=17, washout=20がデフォルト
+        # offset=5, length=103, nb_line=17, washout=20がデフォルト
+
+    def execlusion_quantum(self, offset, length, nb_line, washout,std_element = "29Si"):
+        l = self.csv_list(self.folder)
+
+        for file in l:
+            print(file)
+            gasdata = input('Enter gasblank data:')
+            stddata = input('Enter std data:')
+            concentration = input("Enter intencity data:")
+            elements = self.element_list(l)
+            [self.imaging_quantum(self.folder, file, i, offset, length, nb_line, washout,gasdata,stddata,concentration,std_element = "29Si") for i in elements]
+        self.moving(self.folder)
+
+    def normalize(self, element, base):
+        intmin,intmax,frag = float(),float(),0
+        base_list = glob.glob(self.folder +'/result/*'+base+'_q.xlsx')
+        element_list = glob.glob(self.folder +'/result/*'+element+'_q.xlsx')
+        print(base_list)
+        print(element_list)
+
+        print("normalizing")
+        for i in range(len(base_list)):
+            base_data = pd.read_excel(base_list[i], index_col = 0)
+            element_data = pd.read_excel(element_list[i],index_col = 0)
+            nimage = element_data/base_data
+            if frag == 0:
+                    intmin = nimage.min().min()
+                    intmax = nimage[int(len(nimage)/2)][int(len(nimage[0])/2)]
+                    print(intmax,intmin)
+                    frag = 1
+            outname = self.folder+'/mapping/'+base_list[i].split('/')[-1].split('_')[0]+'_'+element+'_per'+base+'.png'
+                #outname = datalist_c[i].split('/')[-1].split('_')[0] + '_' + outname
+            print(outname)
+            sns.set()
+            plt.style.use('dark_background')
+            sns.heatmap(nimage,cmap='jet',vmin=intmin,vmax=intmax,xticklabels=False,yticklabels=False,cbar=True)
+            plt.tight_layout()
+            plt.savefig(outname)
+            plt.close('all')
+        print("Complete!")
 
     def print_ccf(self, std, threshold):
         warnings.filterwarnings('ignore')
         l = self.csv_list(self.folder)
         elements = self.element_list(l)
+        print(elements)
         for file in l:
 
             #plotlyを使う
--- mineral_main.py
+++ mineral_main.py
@@ -3,4 +3,9 @@ from iquant3d_mineral import *
 
 iq3t_m = iq3t_m("plesovice")
 #iq3t_m.execlusion(offset = 7, length = 39,nb_line = 91,washout = 10)
-iq3t_m.print_ccf(std = "91Zr", threshold = 1000000)
+#iq3t_m.execlusion_quantum(offset = 7, length = 39,nb_line = 91,washout = 10)
+#iq3t_m.print_ccf(std = "91Zr", threshold = 1000000)
+iq3t_m.normalize(element = "232Th",base = "238U")
+iq3t_m.normalize(element = "206Pb",base = "238U")
+iq3t_m.normalize(element = "207Pb",base = "206Pb")
+iq3t_m.normalize(element = "140Ce",base = "172Yb")
